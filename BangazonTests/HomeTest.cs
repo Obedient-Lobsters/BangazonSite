8a675322fda827e8d4da00463f803f5ad01c710e
Shu52 <shusajid@gmail.com>
2018-10-16 17:19:19 -0500

    New clone from master, installed versions 2.1.1 into bangazon test

---
+++ BangazonTests/HomeTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace BangazonTests
+{
+ 
+        public class HomeTest : IClassFixture<WebApplicationFactory<Bangazon.Startup>>
+        {
+            private readonly WebApplicationFactory<Bangazon.Startup> _factory;
+
+            public HomeTest(WebApplicationFactory<Bangazon.Startup> factory)
+            {
+                _factory = factory;
+            }
+        [Theory]
+        [InlineData("/")]
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("text/html; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+    }
+    
+}
