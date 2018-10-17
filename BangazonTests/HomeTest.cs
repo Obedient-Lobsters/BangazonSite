540598bfb5a0b2914e0c92e643bd275f10840bb2
Shu52 <shusajid@gmail.com>
2018-10-17 10:50:47 -0500

    Edited Index.view, added test for page load.

--- BangazonTests/HomeTest.cs
+++ BangazonTests/HomeTest.cs
@@ -8,6 +8,10 @@ using Xunit;
 namespace BangazonTests
 {
  
+        //Author: Shu Sajid
+        //Purpose: This is a testing unit to check if home page loads
+
+        //This method uses Microsoft.AspNetCore.Mvc.Testing and a refrence to the Bangazon.cproj
         public class HomeTest : IClassFixture<WebApplicationFactory<Bangazon.Startup>>
         {
             private readonly WebApplicationFactory<Bangazon.Startup> _factory;
@@ -16,6 +20,7 @@ namespace BangazonTests
             {
                 _factory = factory;
             }
+        //This theory only checks one route
         [Theory]
         [InlineData("/")]
         public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
@@ -27,7 +32,25 @@ namespace BangazonTests
             var response = await client.GetAsync(url);
 
             // Assert
+            //checks header for content that will only be generated on a successful page load
             response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("text/html; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+        //This fact checks the / route to ensure if the page loads and content is displayed on page
+        [Fact]
+        public async Task HomeLoads()
+        {
+            // Arrange
+            var _client = _factory.CreateClient();
+
+            //Act
+            var response = await _client.GetAsync("/");
+            var content = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Matches("&copy; 2018 - Bangazon", content);
             Assert.Equal("text/html; charset=utf-8",
                 response.Content.Headers.ContentType.ToString());
         }
